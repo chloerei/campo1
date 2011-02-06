d2b90e73ba66218216e0a10e68e17f1fa8eee859
Rei <chloerei@gmail.com>
2011-02-06 09:28:39 +0000

    init commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Campo::Application.config.secret_token = '4c8418b11e5529b65f7fff02e2e496f0cbdbac990b8453cfe3c67e747469984945bc10039e542c76cb0f8226a950a2c1d55e816fced1a0d65d27d39bcb4fd427'
