7b7be240b51fcff94305413120705f0373f12352
Rei <chloerei@gmail.com>
2011-02-08 14:37:15 +0000

    claerup top menu.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :current_logined?
   
   protected
 
