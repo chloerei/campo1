3e5396da3f72ebf5a171208d9e1ce74a9b3f4a4e
Rei <chloerei@gmail.com>
2011-02-15 06:37:01 +0000

    modify person page style.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,5 +1,10 @@
 class PeopleController < ApplicationController
   def show
     @person = User.first :conditions => {:username => /^#{params[:username]}$/i}
+    @topics = @person.topics.desc(:created_at).limit(10)
+    user_ids = @topics.map{|topic| topic.last_replied_by_id}.compact.uniq
+    @user_hash = {}
+    users = User.where(:_id.in => user_ids)
+    users.each{|user| @user_hash[user.id] = user}
   end
 end
