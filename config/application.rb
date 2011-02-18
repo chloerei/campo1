6a6ce6d1b4d5553fd811188add2f12f887677eff
Rei <chloerei@gmail.com>
2011-02-18 14:25:59 +0000

    add heroku and mongohq config.

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,18 @@
 require File.expand_path('../boot', __FILE__)
 
+# if use heroku and mongohq
+=begin
+require 'uri'
+if ENV['MONGOHQ_URL']
+  mongo_uri = URI.parse(ENV['MONGOHQ_URL'])
+  ENV['MONGOID_HOST'] = mongo_uri.host
+  ENV['MONGOID_PORT'] = mongo_uri.port.to_s
+  ENV['MONGOID_USERNAME'] = mongo_uri.user
+  ENV['MONGOID_PASSWORD'] = mongo_uri.password
+  ENV['MONGOID_DATABASE'] = mongo_uri.path.gsub('/', '')
+end
+=end
+
 #require 'rails/all'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
