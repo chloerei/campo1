f17c9c1be72e15b3dd26647461c5ff0ab3dfc8e4
Rei <chloerei@gmail.com>
2011-02-27 11:38:25 +0000

    redesign admin layout, base admin dashboard function.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < Admin::BaseController
   def show
+    @today_topics_count = Topic.count :conditions => {:created_at.gt => Date.today.to_time}
+    @today_replies_count = Reply.count :conditions => {:created_at.gt => Date.today.to_time}
+    @today_users_count = User.count :conditions => {:created_at.gt => Date.today.to_time}
   end
 end
