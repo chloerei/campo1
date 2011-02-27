307ea8be573333c5773c755b9188c7826495c409
Rei <chloerei@gmail.com>
2011-02-27 14:46:43 +0000

    admin topics show action.

--- app/controllers/admin/topics_controller.rb
+++ app/controllers/admin/topics_controller.rb
@@ -4,4 +4,8 @@ class Admin::TopicsController < Admin::BaseController
     user_ids = @topics.map{|topic| topic.user_id}.flatten.compact.uniq
     @user_hash = User.create_user_hash(user_ids)
   end
+
+  def show
+    @topic = Topic.find params[:id]
+  end
 end
--- test/functional/admin/topics_controller_test.rb
+++ test/functional/admin/topics_controller_test.rb
@@ -4,6 +4,7 @@ class Admin::TopicsControllerTest < ActionController::TestCase
   def setup
     @user = create_user
     @admin = create_admin
+    @topic = @user.topics.create :title => 'title', :content => 'content', :tags => 'tag1 tag2'
   end
 
   def test_index
@@ -17,5 +18,18 @@ class Admin::TopicsControllerTest < ActionController::TestCase
     login_as @admin
     get :index
     assert_template 'index'
+  end
+
+  def test_show
+    get :show, :id => @topic.id
+    assert_redirected_to login_url
+
+    login_as @user
+    get :show, :id => @topic.id
+    assert_template 'errors/422'
+    
+    login_as @admin
+    get :show, :id => @topic.id
+    assert_template 'show'
   end
 end
