a24664e2c6084a0efaae8341d05f532e711629ba
Rei <chloerei@gmail.com>
2011-02-28 12:24:03 +0000

    don't show user info and topics when it's banned.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,18 +2,22 @@ class PeopleController < ApplicationController
   def show
     @person = User.first :conditions => {:username => /^#{params[:username]}$/i}
     raise Mongoid::Errors::DocumentNotFound.new(User, params[:username]) if @person.nil?
-    set_page_title @person.profile.name
-    @topics = @person.topics.desc(:created_at).limit(10)
-    user_ids = @topics.map{|topic| topic.last_replied_by_id}.compact.uniq
-    @user_hash = User.create_user_hash(user_ids)
+    unless @person.banned?
+      set_page_title @person.profile.name
+      @topics = @person.topics.desc(:created_at).limit(10)
+      user_ids = @topics.map{|topic| topic.last_replied_by_id}.compact.uniq
+      @user_hash = User.create_user_hash(user_ids)
+    end
   end
 
   def topics
     @person = User.first :conditions => {:username => /^#{params[:username]}$/i}
     raise Mongoid::Errors::DocumentNotFound.new(User, params[:username]) if @person.nil?
-    set_page_title @person.profile.name
-    @topics = @person.topics.desc(:created_at).paginate(:per_page => 20, :page => params[:page])
-    user_ids = @topics.map{|topic| topic.last_replied_by_id}.compact.uniq
-    @user_hash = User.create_user_hash(user_ids)
+    unless @person.banned?
+      set_page_title @person.profile.name
+      @topics = @person.topics.desc(:created_at).paginate(:per_page => 20, :page => params[:page])
+      user_ids = @topics.map{|topic| topic.last_replied_by_id}.compact.uniq
+      @user_hash = User.create_user_hash(user_ids)
+    end
   end
 end
