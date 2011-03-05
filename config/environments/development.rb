ed9af24b4ec535e99bd97db145db59a13eaba950
Rei <chloerei@gmail.com>
2011-03-05 09:10:23 +0000

    add smtp config.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,14 @@ Campo::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => '<username>',
+    :password             => '<password>',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,13 @@ Campo::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => '<username>',
+    :password             => '<password>',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
