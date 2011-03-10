73c2c481db292ab831df2cdf696a022f517122f3
Rei <chloerei@gmail.com>
2011-03-10 08:00:07 +0000

    fix bug: notification.user conflict, rename to notification.reply_user .

--- app/models/notification/mention.rb
+++ app/models/notification/mention.rb
@@ -1,5 +1,5 @@
 class Notification::Mention < Notification::Base
   referenced_in :topic
   referenced_in :reply
-  referenced_in :user
+  referenced_in :reply_user, :class_name => 'User'
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -41,7 +41,7 @@ class Reply
 
   def send_memetion_notifications
     memtion_users.each do |user|
-      user.send_notification({:user_id  => self.user_id,
+      user.send_notification({:reply_user_id  => self.user_id,
                               :topic_id => self.topic_id,
                               :reply_id => self.id,
                               :text     => self.content.slice(0..99)}, Notification::Mention)
