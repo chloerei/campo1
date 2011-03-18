566ae726bdf363ea547cf7f5c5422cdaef854c0e
Rei <chloerei@gmail.com>
2011-03-18 12:04:06 +0000

    add close button in flash notice box.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function () {
+  $('.flash .close').click(function() {
+    $(this).parent('.flash').hide('blind').remove();
+  });
+});
