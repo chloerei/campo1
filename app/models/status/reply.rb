94a7bcb385fbfb3d1b4227d14299400d0197cd32
Rei <chloerei@gmail.com>
2011-06-15 20:00:05 +0800

    no send status_reply if status content start with '@'

--- app/models/status/reply.rb
+++ app/models/status/reply.rb
@@ -4,6 +4,14 @@ class Status::Reply < Status::Base
   validates_presence_of :topic_id, :reply_id
 
   def target_user_ids
-    [topic.marker_ids.to_a, topic.user_id, user.follower_ids].flatten.uniq - [user_id]
+    unless targeted?
+      [topic.marker_ids.to_a, topic.user_id, user.follower_ids].flatten.uniq - [user_id]
+    else
+      []
+    end
+  end
+
+  def targeted?
+    reply.mention_user_ids.any? and reply.content[0] == '@'
   end
 end
--- test/unit/status/reply_test.rb
+++ test/unit/status/reply_test.rb
@@ -24,6 +24,27 @@ class Status::ReplyTest < ActiveSupport::TestCase
     assert status_reply.target_user_ids.include?(follower.id)
   end
 
+  test "should no send if reply is targeted" do
+    status_reply = Factory :status_reply
+    assert !status_reply.targeted?
+    status_reply.reply.content = '@user'
+    status_reply.reply.save
+    assert !status_reply.targeted?
+
+    user = Factory :user
+    status_reply.reply.content = "@#{user.username}"
+    status_reply.reply.save
+    assert status_reply.targeted?
+
+    assert status_reply.target_user_ids.empty?
+    status_reply.topic.mark_by Factory(:user)
+    status_reply.topic.reply_by Factory(:user)
+    status_reply.topic.reload
+    status_reply.user.add_follower Factory(:user)
+    status_reply.user.reload
+    assert status_reply.target_user_ids.empty?
+  end
+
   test "should no send stream to whom create reply" do
     user = Factory :user
 
